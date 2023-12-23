d66d0e849f96157cbe359bbb421d77f9a7ac4ffd
kavindu919 <jayakodyk2@gmail.com>
2023-12-23 15:40:36 +0530

    connection established for the data base

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv' // here this use because use of .env file with out this server goes error
+dotenv.config(); // initalized it 
+// connect mongodb like this reason to hide password .env files ignored when uploding git hub
+
+mongoose.connect(process.env.MONGO) 
+.then(()=>{
+    console.log("Connected to the MongoDB!!!")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
+
 
 const app = express()
 
