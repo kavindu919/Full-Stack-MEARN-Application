35779236b45aa82b890193ec0d35046625ad7525
kavindu919 <jayakodyk2@gmail.com>
2023-12-23 21:26:25 +0530

    create test api route

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv' // here this use because use of .env file with out this server goes error
+import userRouter from './routes/user.routes.js'
+
 dotenv.config(); // initalized it 
 // connect mongodb like this reason to hide password .env files ignored when uploding git hub
 
@@ -13,10 +15,9 @@ mongoose.connect(process.env.MONGO)
 })
 
 
-
-
 const app = express()
 
 app.listen(3000, ()=>{
     console.log("Server is Runnig on port 3000")
 })
+app.use("/api/user",userRouter)
