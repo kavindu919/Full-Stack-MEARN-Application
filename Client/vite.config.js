27014f8935643e0a93680d3509be599a6024781a
kavindu919 <jayakodyk2@gmail.com>
2023-12-25 20:20:40 +0530

    complet sign up page,proxiy and error handle to the page

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //create proxy
+  server: {
+    proxy: {
+      '/api':{
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
