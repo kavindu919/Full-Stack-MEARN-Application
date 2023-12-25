3d00c19dfbc45308ac2a45cd7e214b52bfeeef60
kavindu919 <jayakodyk2@gmail.com>
2023-12-25 08:52:19 +0530

    create middleware and error handler function

---
+++ Api/utils/error.js
@@ -0,0 +1,7 @@
+//create function to handle errors
+export const errorHandler = (statusCode,message)=>{
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+}
