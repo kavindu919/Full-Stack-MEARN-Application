d18ac59374fc3b6f998a8a698da9921917024afd
kavindu919 <jayakodyk2@gmail.com>
2024-01-21 21:49:12 +0530

    complete update listing functions

--- Api/controllars/listning.controller.js
+++ Api/controllars/listning.controller.js
@@ -53,3 +53,16 @@ export const updateListing = async (req,res,next) => {
     next(error)
   }
 }
+//create function for getListing api 
+export const getListing = async (req,res,next) => {
+  try {
+    const listing = await Listing.findById(req.params.id)
+    //cheak listing exsists
+    if (!listing) {
+      return next(errorHandler(404,'Listing not found'))
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error)
+  }
+}
--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,6 +8,7 @@ import SignUp from './pages/Signup'
 import Header from './components/Header'
 import PrivateRouts from './components/PrivateRouts'
 import CreateListing from './pages/CreateListing'
+import UpdateListing from './pages/UpdateListing'
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
         <Route element = {<PrivateRouts/>}>
         <Route path='/profile' element ={<Profile/>}/>
         <Route path='/create-listing' element ={<CreateListing/>}/>
+        <Route path='/update-listing/:listingId' element ={<UpdateListing/>}/>
         </Route>
       </Routes>
      </BrowserRouter>
